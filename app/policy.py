c337b94bf64faa128825e492f5026fa4b0eca4d8
athipan1 <166430089+athipan1@users.noreply.github.com>
2026-06-18 23:51:20 +0700

    Add risk policy

---
+++ app/policy.py
@@ -0,0 +1,13 @@
+MAX_POSITION_PCT = 0.10
+MAX_TRADE_LOSS_PCT = 0.01
+MAX_TOTAL_EXPOSURE_PCT = 1.00
+MAX_MARGIN_MULTIPLIER = 1.00
+MIN_PROTECTION_DISTANCE_PCT = 0.002
+
+POLICY = {
+    'max_position_pct': MAX_POSITION_PCT,
+    'max_trade_loss_pct': MAX_TRADE_LOSS_PCT,
+    'max_total_exposure_pct': MAX_TOTAL_EXPOSURE_PCT,
+    'max_margin_multiplier': MAX_MARGIN_MULTIPLIER,
+    'min_protection_distance_pct': MIN_PROTECTION_DISTANCE_PCT,
+}
